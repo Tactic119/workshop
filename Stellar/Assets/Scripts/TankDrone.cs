aa23165caf1c9206d01dcd103d87397e8c7782b6
Storm0799 <pastyboi_689@yahoo.com>
2025-03-28 18:52:55 -0500

    hjb

--- Stellar/Assets/Scripts/TankDrone.cs
+++ Stellar/Assets/Scripts/TankDrone.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using static UnityEngine.GraphicsBuffer;
 
@@ -10,16 +11,37 @@ public class TankDrone : MonoBehaviour
     public GameObject tankDrone;
     public int state;
     public bool atPosition;
-    Vector3 targetPosition;
-    private float speed;
+    private Vector3 targetPosition;
+    private Vector3 enemyPosition;
+    public float speed;
     public bool hacked;
+
+    public float sightDistance;
+    public float fieldOfView;
+    public GameObject Player;
+    public bool foundTarget;
+    public float agroTimer;
+    public float attackTimer;
+    public float attackDistance;
+    public float gunAngle;
+    public GameObject gunRotation;
 
     void Start()
     {
         atPosition = true;
-        speed = 1f;
+        speed = 1.3f;
         state = 1;
         hacked = false;
+        foundTarget = false;
+        agroTimer = 0f;
+        attackTimer = 0f;
+
+        sightDistance = 7f;
+        fieldOfView = 60f;
+        attackDistance = 3.5f;
+
+        gunAngle = 0f;
+        //gunRotation.transform.rotation = new Vector3(gunRotation.transform.rotation.x, gunRotation.transform.rotation.y, gunAngle);
     }
 
 
@@ -34,18 +56,45 @@ public class TankDrone : MonoBehaviour
         else if (state == 3)
             AttackTarget();
 
-        AvoidObstacles();
+        if(state >= 1)Search();
 
+        if (agroTimer > 0)
+            agroTimer -= Time.deltaTime;
+        else if (agroTimer <= 0)
+        {
+            state = 1;
+            foundTarget = false;
+        }
+    }
+
+    public void Search()
+    {
+        if (hacked == true)enemyPosition = new Vector3(0, 0, 0);
+        else if (hacked == false)
+            enemyPosition = Player.transform.position;
+
+        if (Vector3.Distance(transform.position, enemyPosition) < sightDistance)
+        {
+            Vector3 targetDirection = enemyPosition - transform.position;
+            float angleToTarget = Vector3.Angle(targetDirection, transform.forward);
+            if (angleToTarget >= -fieldOfView && angleToTarget <= fieldOfView)
+            {
+                Ray ray = new Ray(transform.position, targetDirection);
+                Debug.DrawRay(ray.origin, ray.direction * sightDistance);
+
+                foundTarget = true;
+                agroTimer = 5f;
+                if (state == 1)
+                {
+                    state = 2;
+                }
+            }
+        }
     }
 
     public void Idle()
     {
         // idle
-    }
-
-    public void SwitchToAttack()
-    {
-
     }
 
     public void Wandering()
@@ -76,8 +125,20 @@ public class TankDrone : MonoBehaviour
 
     public void MoveToTarget()
     {
+        speed = 1.8f;
+        FindEnemyPosition();
 
+        if (Vector3.Distance(transform.position, enemyPosition) > attackDistance) transform.position = Vector3.MoveTowards(transform.position, enemyPosition, speed * Time.deltaTime);
+        else if (Vector3.Distance(transform.position, enemyPosition) <= attackDistance) state = 3;
+
+        transform.LookAt(enemyPosition);
     }
+
+    public void FindEnemyPosition()
+    {
+        enemyPosition = new Vector3(Player.transform.position.x, transform.position.y, Player.transform.position.z);
+    }
+       
 
     public void AttackTarget()
     {
